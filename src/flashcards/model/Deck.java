185a959ac3154d5f84c2e13e131aa56d9604aaaf
Clément Joly <clement.joly@telecomnancy.eu>
2018-12-17 15:44:18 +0100

    Ajout classe Deck

--- src/flashcards/model/Deck.java
+++ src/flashcards/model/Deck.java
@@ -1,4 +1,41 @@
 package flashcards.model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 public class Deck {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField
+    private String nom;
+    @DatabaseField
+    private String  description;
+
+    public Deck() {}
+
+    public Deck(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
