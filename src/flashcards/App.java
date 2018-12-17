33dd79a0c5c5fdc47ff938a6aaf17715aeac4fa5
Lucas Fenouillet <lucas.fenouillet@telecomnancy.eu>
2018-12-17 18:30:39 +0100

    Liaison des vues des différents onglets

--- src/flashcards/App.java
+++ src/flashcards/App.java
@@ -1,9 +1,11 @@
 package flashcards;
 
+import flashcards.controllers.Tabs;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -11,11 +13,26 @@ import java.net.URL;
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        URL path = getClass().getClassLoader().getResource("PacketSearch.fxml");
-        Parent root = FXMLLoader.load(path);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 700));
+    public void start(Stage primaryStage) throws Exception {
+        URL path;
+
+        BorderPane root = new BorderPane();
+
+        Tabs o = new Tabs(root);
+
+        FXMLLoader loader = new FXMLLoader();
+        URL coucou = getClass().getClassLoader().getResource("Tabs.fxml");
+        loader.setLocation(coucou);
+        loader.setControllerFactory(iC -> o);
+        root.setTop(loader.load());
+
+
+        path = getClass().getClassLoader().getResource("Packets.fxml");
+        Parent decks = FXMLLoader.load(path);
+        root.setCenter(decks);
+
+        primaryStage.setTitle("Memscarlo");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
