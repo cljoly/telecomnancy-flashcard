0a78270b68aaceb371c706bf1ddf5018d6fb8a3e
Laury de Donato <laury.de-donato@telecomnancy.eu>
2018-12-17 13:27:58 +0100

    Architecture propre

---
+++ src/flashcards/App.java
@@ -0,0 +1,29 @@
+package flashcards;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        URL path = getClass().getClassLoader().getResource("AppView.fxml");
+        Parent root = FXMLLoader.load(path);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+}
