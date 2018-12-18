923f7ffda8c1466971d041ef4555d9734627d262
JOLY Clément <clement.joly@telecomnancy.eu>
2018-12-18 15:40:37 +0100

    Ajout des états de frimouses dans l’interface

---
+++ src/flashcards/model/Faces.java
@@ -0,0 +1,13 @@
+package flashcards.model;
+
+/**
+ * Frimousses utilisées dans l’interface
+ */
+public enum Faces {
+    /** :-) */
+    FaceSmile,
+    /** :-| */
+    FaceNeutral,
+    /** :-( */
+    FaceFrown
+}
