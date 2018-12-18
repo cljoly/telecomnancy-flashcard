c7cb16b3b68eeeaeb99da555b4f73c05448c22b9
Laury de Donato <laury.de-donato@telecomnancy.eu>
2018-12-18 10:58:43 +0100

    Changements dans créer un paquet

--- src/flashcards/controllers/DeckCreate.java
+++ src/flashcards/controllers/DeckCreate.java
@@ -3,6 +3,7 @@ package flashcards.controllers;
 import flashcards.model.GameUsers;
 import flashcards.model.User;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -14,6 +15,8 @@ public class DeckCreate {
     private TextField deck_name;
     @FXML
     private TextArea deck_description;
+    @FXML
+    private Label deck_create_msg;
 
     public DeckCreate(){}
 
@@ -23,12 +26,13 @@ public class DeckCreate {
         try {
             user.create_deck(deck_name.getText(), deck_description.getText());
             System.out.println("Paquet créé et ajouté à la BD");
-            //TODO changer message quand deck ajoutée, embêtant car utilise des threads
+            deck_create_msg.setText("Paquet \"" + deck_name.getText() + "\" créé avec succès !");
         } catch (SQLException e){
             //TODO popup utilisateur
         }
         clean_fields();
     }
+
 
     public void clean_fields(){
         deck_name.clear();
