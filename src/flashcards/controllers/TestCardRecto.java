fb7c57b57afcafdc3175d88890db3ed003f8bc4b
Laury de Donato <laury.de-donato@telecomnancy.eu>
2018-12-19 15:17:29 +0100

    Passage au verso de la carte/affichage de la réponse

--- src/flashcards/controllers/TestCardRecto.java
+++ src/flashcards/controllers/TestCardRecto.java
@@ -3,9 +3,12 @@ package flashcards.controllers;
 import flashcards.model.Card;
 import flashcards.model.Training;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,20 +22,41 @@ public class TestCardRecto implements Initializable {
     private Button btn_test_card_show;
     @FXML
     private Label lbl_test_card_recto_a;
+    @FXML
+    private AnchorPane deck_recto_window;
 
     public TestCardRecto(Training training){
         this.training = training;
+        if (this.training != null) {
+            this.card = this.training.go_to_next_card();
+        } else {
+            this.card = null;
+        }
     }
 
     public void when_show_answer_is_clicked(){
+        try {
 
+            FXMLLoader verso = new FXMLLoader();
+            verso.setLocation(getClass().getClassLoader().getResource("TestCardVerso.fxml"));
+            verso.setControllerFactory(iC -> new TestCardVerso(this.training, this.card));
+
+            //chargement de l'anchor pane deck review
+            AnchorPane content = verso.load();
+
+            //lien
+            ((BorderPane) deck_recto_window.getParent().getScene().getRoot()).setCenter(content);
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.training != null) {
-            Card c = this.training.go_to_next_card();
-            lbl_test_card_recto_a.setText(c.getRecto());
+        if (this.training != null){
+            lbl_test_card_recto_a.setText(this.card.getRecto());
         } else {
         //Card c = new Card("Toto", "Tata", false);
         //String recto = c.getRecto();
--- src/flashcards/controllers/TestCardVerso.java
+++ src/flashcards/controllers/TestCardVerso.java
@@ -1,4 +1,41 @@
 package flashcards.controllers;
 
-public class TestCardVerso {
+import flashcards.model.Card;
+import flashcards.model.Training;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TestCardVerso implements Initializable{
+
+    private Training training;
+    private Card card;
+
+    @FXML
+    private Label lbl_test_card_verso_a;
+    @FXML
+    private Label lbl_test_card_verso_b;
+
+    public TestCardVerso(Training training, Card card){
+        this.training = training;
+        this.card = card;
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (this.training != null && this.card != null){
+            lbl_test_card_verso_a.setText(this.card.getRecto());
+            lbl_test_card_verso_b.setText(this.card.getVerso());
+        } else {
+            //Card c = new Card("Toto", "Tata", false);
+            //String recto = c.getRecto();
+            lbl_test_card_verso_a.setText("Ne marche pas");
+            lbl_test_card_verso_b.setText("Ne marche pas");
+        }
+
+    }
 }
