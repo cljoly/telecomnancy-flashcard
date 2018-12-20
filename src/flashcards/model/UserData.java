f751ad9e4fbb19eae4224d23545502114bc20a4d
JOLY Clément <clement.joly@telecomnancy.eu>
2018-12-20 16:27:17 +0100

    Javadoc : UserData

--- src/flashcards/model/UserData.java
+++ src/flashcards/model/UserData.java
@@ -5,12 +5,20 @@ import com.google.gson.Gson;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Classe qui regroupe les données avant export.
+ */
 public class UserData
 {
     private List<Card> cards;
     private List<Deck> decks;
     private List<DeckCard> links;
 
+    /**
+     * Récupère les données de l’itilisateur, sauf les statistiques.
+     * @param u Utilisateur dont on veut récupérer les données
+     * @throws SQLException
+     */
     public UserData(User u) throws SQLException
     {
         this.cards = u.get_all_cards();
