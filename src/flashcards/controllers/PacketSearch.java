7d3914c8d24317105b1b73f5061c0122ecb66670
Laury de Donato <laury.de-donato@telecomnancy.eu>
2018-12-20 12:33:49 +0100

    Première partie de la liaison de l'affichage des cartes d'un paquet

---
+++ src/flashcards/controllers/PacketSearch.java
@@ -0,0 +1,51 @@
+package flashcards.controllers;
+
+import flashcards.model.Deck;
+import flashcards.model.GameUsers;
+import flashcards.model.User;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class PacketSearch implements Initializable {
+
+    @FXML
+    private VBox list_of_all_decks;
+
+    private List<Deck> listOfAllDecks;
+    private User currentUser;
+
+
+    public PacketSearch(){
+        this.currentUser = GameUsers.getInstance().getCurrentUser();
+
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try{
+            this.listOfAllDecks = this.currentUser.get_all_decks();
+            for (Deck d : this.listOfAllDecks){
+                Button b = new Button();
+                b.setText(d.getNom());
+                b.setMinWidth(250);
+                b.setPrefWidth(29);
+
+                b.addEventHandler(ActionEvent.ACTION, new WhenADeckIsClicked(b));
+
+                list_of_all_decks.getChildren().add(b);
+            }
+        } catch (SQLException e){}
+
+    }
+
+}
